f4665d06e694d69999faa8c392f23485ce73e12a
Injae Ryou <sts07142@naver.com>
2023-05-21 17:37:32 +0900

    당근 버리고 무당벌레 넣기

--- 2023-karrot-challenge-main/src/main/java/numble/karrot/image_upload/config/WebMvcConfiguration.java
+++ 2023-karrot-challenge-main/src/main/java/numble/karrot/image_upload/config/WebMvcConfiguration.java
@@ -1,11 +1,11 @@
-// package numble.karrot.image_upload.config;
+ package numble.karrot.image_upload.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebMvcConfiguration implements WebMvcConfigurer {
+ @Configuration
+ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
