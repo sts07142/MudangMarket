a0fa6be773bef49749456cb50a543383be9a1e6c
Injae Ryou <sts07142@naver.com>
2023-05-21 17:33:32 +0900

    당근 버리고 무당벌레 넣기

--- 2023-karrot-challenge-main/src/main/java/numble/karrot/member/domain/MemberImageInit.java
+++ 2023-karrot-challenge-main/src/main/java/numble/karrot/member/domain/MemberImageInit.java
@@ -1,5 +1,5 @@
 package numble.karrot.member.domain;
 
 public class MemberImageInit {
-   public static final String INIT_URL = "https://pbs.twimg.com/profile_images/1463023431684079616/ghuPttFw_400x400.jpg";
+   public static final String INIT_URL = "https://i.ibb.co/Mg9k2BN/free-icon-ladybug-7963466.png";
 }
