bbcd91e50ae4e6aabb1adb66c1d0c11c28dfc45a
HyunseoPark <sspp8585@icloud.com>
2023-05-21 17:31:52 +0900

    프런트 마이페이지, 관심목록 변경

--- 2023-karrot-challenge-main/src/main/java/numble/karrot/image_upload/config/WebMvcConfiguration.java
+++ 2023-karrot-challenge-main/src/main/java/numble/karrot/image_upload/config/WebMvcConfiguration.java
@@ -1,14 +1,14 @@
-package numble.karrot.image_upload.config;
+// package numble.karrot.image_upload.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebMvcConfiguration implements WebMvcConfigurer {
+// @Configuration
+// public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("file:///C:/Users/User/IdeaProjects/img_test/");
-    }
-}
+//     @Override
+//     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//         registry.addResourceHandler("/**").addResourceLocations("file:///C:/Users/User/IdeaProjects/img_test/");
+//     }
+// }
